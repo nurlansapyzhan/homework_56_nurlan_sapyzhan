a187db49ff4e395a9e76a861ef6fbfea37561afd
Nurlan Sapyzhan <nurlan_sapyzhan@mail.ru>
2023-02-24 23:30:11 +0600

    Файлы проекта и модель
    Добавил файлы проекта и создал модель продуктов. Зарегестрировал модель в админке. Создал фикстуры

---
+++ source/labwork_56/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False, verbose_name='Наименование товара')
+    description = models.CharField(max_length=2000, null=True, verbose_name='Описание товара')
+    image = models.TextField(null=False, verbose_name='Фото товара')
+    category = models.CharField(max_length=50, null=False, default='other', verbose_name='Категория')
+    amount = models.IntegerField(null=False, default=1, verbose_name='Остаток')
+    price = models.DecimalField(max_digits=7, decimal_places=2)
